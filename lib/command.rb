38a8a5aad6afad4073e1117b871cd4449437bf24
pucm33b4 <pucm33b4@s.okayama-u.ac.jp>
2026-06-29 14:50:10 +0900

    fix: Commandの初期化処理を追加

--- lib/command.rb
+++ lib/command.rb
@@ -7,6 +7,10 @@ class Command
   # コマンドが正常終了したときに返す共通の成功コード。
   SUCCESS = 0
 
+  def initialize
+    # Commandは初期化時に受け取る値を持たない。
+  end
+
   def execute
     # コマンド固有の処理とCommandResultの返却は，各サブクラスのexecuteで実装する。
     raise NotImplementedError
